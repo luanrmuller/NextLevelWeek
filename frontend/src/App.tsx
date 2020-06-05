cf853365e93c05eadf4d08cfa4c37b792573945a
luanrmuller <muller@live.in>
2020-06-05 00:41:10 -0200

    feat: front ecoleta

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Header from './pages/Header';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
